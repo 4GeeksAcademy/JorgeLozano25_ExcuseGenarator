9920980613d603e4066e5865e368f6a87e1657d9
jorgelozano25 <jorge_9dealmeria@hotmail.com>
2024-08-15 12:30:58 +0000

    excuse generator terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generarExcusa();
+  });
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generarExcusa = () => {
+  let sust = [
+    "el cartero ",
+    "un pulpo ",
+    "un perro ",
+    "papá ",
+    "un payaso ",
+    "la abuela ",
+    "un policia ",
+    "el presidente "
+  ];
+  let accion = [
+    "cogió mi ",
+    "tiró mi ",
+    "orinó mi ",
+    "pateó mi ",
+    "robó mi ",
+    "quemó mi ",
+    "mordió mi ",
+    "golpeó mi "
+  ];
+  let objeto = [
+    "dedo gordo del pie ",
+    "bicicleta ",
+    "reloj ",
+    "zapato ",
+    "cartera ",
+    "camiseta ",
+    "nintendo ",
+    "ordenador ",
+    "telefono ",
+    "bocadillo "
+  ];
+  let lugar = [
+    "en la calle",
+    "en el colegio",
+    "en la carretera",
+    "frente a la tienda de comics",
+    "cerca del centro comercial",
+    "cerca del baño",
+    "en la estacion de autobus"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // variables random
+  let rdm1 = Math.floor(Math.random() * sust.length);
+  let rdm2 = Math.floor(Math.random() * accion.length);
+  let rdm3 = Math.floor(Math.random() * objeto.length);
+  let rdm4 = Math.floor(Math.random() * lugar.length);
+
+  return (
+    sust[rdm1] + " " + accion[rdm2] + " " + objeto[rdm3] + " " + lugar[rdm4]
+  );
 };
